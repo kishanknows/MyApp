c7558c4f3f3874120c9ceb61f301a629d1c9d11a
Kishan Jangde <kishanknows@gmail.com>
2023-04-04 00:16:14 +0530

    added tab navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import {StyleSheet, Text, View} from 'react-native';
-import MyButton from './src/components/my-button';
-import Weather from './src/components/weather';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ProfileScreen from './src/components/profile';
+import HomeScreen from './src/components/home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import WeatherScreen from './src/components/weather';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Weather />
-      <MyButton onPress={() => {}} />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{tabBarShowLabel: false}}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'MyApp',
+            tabBarIcon: props => {
+              return props.focused ? (
+                <Ionicon name="home" size={25} color="black" />
+              ) : (
+                <Ionicon name="home-outline" size={25} color="black" />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={WeatherScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: props => {
+              return props.focused ? (
+                <Ionicon name="search" size={25} color="black" />
+              ) : (
+                <Ionicon name="search-outline" size={25} color="black" />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            headerTitleAlign: 'center',
+            tabBarIcon: props => {
+              return props.focused ? (
+                <Ionicon name="person" size={25} color="black" />
+              ) : (
+                <Ionicon name="person-outline" size={25} color="black" />
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
