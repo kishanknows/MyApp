787f3124d9bea6d11198abce556e274e85cba459
Kishan Jangde <kishanknows@gmail.com>
2023-04-04 00:18:01 +0530

    created custom hook useLocalStorage

---
+++ src/hooks/local-storage.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect, useState} from 'react';
+
+const storeData = async <S>(key: string, value: S) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getData = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const useLocalStorage = <S>(key: string, value: S): [S, (value: S) => void] => {
+  const [getter, setGetter] = useState<S>(value);
+  const setter = (value: S) => {
+    setGetter(value);
+    storeData(key, value);
+  };
+  useEffect(() => {
+    getData(key)
+      .then(fetchedValue => fetchedValue && setGetter(fetchedValue))
+      .catch(error => setter(value));
+  }, []);
+  return [getter, setter];
+};
+
+export default useLocalStorage;
